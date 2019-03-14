4b99ec936723871fc31edc4a89df21666a034476
VitalMos <moiseenokv@gmail.com>
2019-03-14 11:59:10 +0300

    add triangle function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  module.exports = function getLoveTrianglesCount(preferences = []){
+    count=0;
+    for(i=0; i<=preferences.length-1; i++) {
+    
+    firstVal = preferences[i];
+    firstInd = i;
+    
+    secondVal = preferences[firstVal-1];
+    secondInd = firstVal-1;
+
+    thirdVal = preferences[secondVal-1];
+    thirdInd = secondVal-1;
+
+    if((firstVal)==(secondInd+1) && (secondVal)==(thirdInd+1) && (thirdVal)==(firstInd+1) ){
+        if(firstVal==secondVal && secondVal==thirdVal){
+            continue;
+        }else{
+            count=count+1;
+        }
+        
+    }
+
+  }
+  return (count/3);
+  };
